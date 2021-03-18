83ef1550b21e34881f05e96787182d4ae2df4bff
mzakirahman <mzakiriau@gmail.com>
2021-03-19 01:52:54 +0700

    Lamda Expression

--- bin/codingdasardart.dart
+++ bin/codingdasardart.dart
@@ -16,11 +16,21 @@ String say2(String from, String messege, [String to, String appName]) {
       ((appName != null) ? " via " + appName : "");
 }
 
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
+
+int doMathOperator(int number1, int number2, Function(int, int) operator) {
+  return operator(number1, number2);
+}
+
 main(List<String> arguments) {
+  Function f;
+  f = luas_segiempat;
+
   print(say("jonny", "hallo", appName: "Whatsapp "));
   print(say2(
     "jonny",
     "hallo",
     "Doloris",
   ));
+  print(doMathOperator(1, 2, (a, b) => a * b));
 }
