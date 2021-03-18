bf1478870bd6fff877144ea6f0b75242fde4a54a
mzakirahman <mzakiriau@gmail.com>
2021-03-19 02:31:29 +0700

    Unary Operator ++/--

--- bin/codingdasardart.dart
+++ bin/codingdasardart.dart
@@ -1,17 +1,8 @@
 import 'dart:io';
 
 main(List<String> arguments) {
-  int i = 5;
-  while (i > 1) {
-    print("hallo ke " + i.toString());
-    i--;
-  }
-
-  print("=====================");
-
-  int x = 0;
-  do {
-    print("dodododo ke " + x.toString());
-    x += 1;
-  } while (x < 5);
+  int a, b;
+  a = 10;
+  b = ++a;
+  print(a.toString() + " - " + b.toString());
 }
