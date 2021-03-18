3502960799ecd5cc78504cb983ab8ed3d246abd8
mzakirahman <mzakiriau@gmail.com>
2021-03-19 01:35:19 +0700

    function default parameter

--- bin/codingdasardart.dart
+++ bin/codingdasardart.dart
@@ -1,19 +1,26 @@
 import 'dart:io';
 
-double luas_segiempat(double panjang, double lebar) {
-  return panjang * lebar;
+String say(String from, String messege, {String to, String appName}) {
+  return from +
+      " say " +
+      messege +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
 }
 
-void sapa_penonton() {
-  print("hallo penonton");
+String say2(String from, String messege, [String to, String appName]) {
+  return from +
+      " say " +
+      messege +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
 }
 
 main(List<String> arguments) {
-  double p, l, luas;
-
-  p = double.tryParse(stdin.readLineSync());
-  l = double.tryParse(stdin.readLineSync());
-  // luas = luas_segiempat(p, l);
-
-  print(luas_segiempat(p, l));
+  print(say("jonny", "hallo", appName: "Whatsapp "));
+  print(say2(
+    "jonny",
+    "hallo",
+    "Doloris",
+  ));
 }
