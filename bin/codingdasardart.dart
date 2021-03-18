9f5a990df851bd230e393eaf3196cd035d780cca
mzakirahman <mzakiriau@gmail.com>
2021-03-19 00:09:39 +0700

    if untuk nilai positif atau negatif

--- bin/codingdasardart.dart
+++ bin/codingdasardart.dart
@@ -2,10 +2,13 @@ import 'dart:io';
 
 main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
-  if (number > 0)
-    print("positif");
-  else if (number < 0)
-    print("negatif");
-  else
-    print("nol");
+  String output;
+  // output = (number > 0) ? "positif" : "negatif atau nol";
+  // if (number > 0) {
+  //   output = "positif";
+  // } else {
+  //   output = "negatif";
+  // }
+
+  print((number > 0) ? number : number * -1);
 }
