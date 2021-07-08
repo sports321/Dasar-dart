deef615c1396bfb41e2548b4c7fd6ac4deb6b48f
sports321 <sicilyvonclaude321@gmail.com>
2021-07-08 14:39:39 +0700

    commit

--- bin/codingdasardart.dart
+++ bin/codingdasardart.dart
@@ -4,7 +4,7 @@ main(List<String> arguments) {
   List<int> myList = [1, 1, 6, 4, 3, 6, 2];
   List<String> list = [];
 
-  list = myList.map((number) => "angkaa" + number.toString()).toList();
+  list = myList.map((number) => "angka" + number.toString()).toList();
   list.forEach((str) {
     print(str);
   });
